5068b0bae1c7c1f3d0f561af4ca82c88a6221c13
Arno Strouwen <arno.strouwen@telenet.be>
2024-01-13 20:00:08 +0100

    More CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,42 +1,18 @@
-using GlobalSensitivity, SafeTestsets
-using Test
+using SafeTestsets, Test
 
 const GROUP = get(ENV, "GROUP", "All")
 
 @time begin
     if GROUP == "All" || GROUP == "GSA"
-        @time @safetestset "Quality Assurance" begin
-            include("qa.jl")
-        end
-        @time @safetestset "Morris Method" begin
-            include("morris_method.jl")
-        end
-        @time @safetestset "Sobol Method" begin
-            include("sobol_method.jl")
-        end
-        @time @safetestset "DGSM Method" begin
-            include("DGSM.jl")
-        end
-        @time @safetestset "eFAST Method" begin
-            include("eFAST_method.jl")
-        end
-        @time @safetestset "RegressionGSA Method" begin
-            include("regression_sensitivity.jl")
-        end
-        @time @safetestset "DeltaMoment Method" begin
-            include("delta_method.jl")
-        end
-        @time @safetestset "Fractional factorial method" begin
-            include("fractional_factorial_method.jl")
-        end
-        @time @safetestset "Rbd-fast method" begin
-            include("rbd-fast_method.jl")
-        end
-        @time @safetestset "Easi Method" begin
-            include("easi_method.jl")
-        end
-        @time @safetestset "Shapley Method" begin
-            include("shapley_method.jl")
-        end
+        @time @safetestset "Morris Method" include("morris_method.jl")
+        @time @safetestset "Sobol Method" include("sobol_method.jl")
+        @time @safetestset "DGSM Method" include("DGSM.jl")
+        @time @safetestset "eFAST Method" include("eFAST_method.jl")
+        @time @safetestset "RegressionGSA Method" include("regression_sensitivity.jl")
+        @time @safetestset "DeltaMoment Method" include("delta_method.jl")
+        @time @safetestset "Fractional factorial method" include("fractional_factorial_method.jl")
+        @time @safetestset "Rbd-fast method" include("rbd-fast_method.jl")
+        @time @safetestset "Easi Method" include("easi_method.jl")
+        @time @safetestset "Shapley Method" include("shapley_method.jl")
     end
 end
