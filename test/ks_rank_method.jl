e92c3043e7af9a3fa103cd6f31699a39f6c65eba
max-de-rooij <54850292+max-de-rooij@users.noreply.github.com>
2024-07-16 13:38:23 +0200

    Update tests
    KSRank is stochastic and slight variations in the outcome measures are possible. New tests check whether parameters are correctly identified as sensitive.

--- test/ks_rank_method.jl
+++ test/ks_rank_method.jl
@@ -26,15 +26,18 @@ end
 lb = -ones(4) * π
 ub = ones(4) * π
 
+
+
 res1 = gsa(ishi, KSRank(n_dummy_parameters=50), [[lb[i], ub[i]] for i in 1:4], samples = 100_000)
 res2 = gsa(ishi_batch, KSRank(), [[lb[i], ub[i]] for i in 1:4], samples = 100_000, batch = true)
 
-@test res1.S ≈ [0.348, 0.167, 0.00388, 0.00671] atol=1e-2
-@test res2.S ≈ [0.348, 0.167, 0.00388, 0.00671] atol=1e-2
+@test (2*res1.Sd[1] .> res1.S) == [0,0,1,1]
+@test (2*res1.Sd[1] .> res2.S) == [0,0,1,1]
+
 
 res1 = gsa(linear, KSRank(), [[lb[i], ub[i]] for i in 1:4], samples = 100_000)
 res2 = gsa(linear_batch, KSRank(), [[lb[i], ub[i]] for i in 1:4], batch = true,
     samples = 100_000)
-@test res1.S ≈ [0.993, 0.00611, 0.00399, 0.00519] atol=1e-2
-@test res2.S ≈ [0.993, 0.00611, 0.00399, 0.00519] atol=1e-2
+@test (2*res1.Sd[1] .> res1.S) == [0,1,1,1]
+@test (2*res1.Sd[1] .> res2.S) == [0,1,1,1]
 
